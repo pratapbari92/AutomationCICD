2f6e4e4c9925c42f6494fa6f8cfdd3bd754da411
Hp <pratapbari0209@gmail.com>
2025-11-27 17:57:37 +0530

    Removed unused test code from src/test/java

---
+++ src/main/java/LoginFacebook/FacebookLogin.java
@@ -0,0 +1,33 @@
+package LoginFacebook;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FacebookLogin {
+
+
+		public static void main(String[] args) {
+			
+	WebDriverManager.chromedriver().setup();
+			
+			WebDriver driver =new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.manage().window().maximize();
+			driver.get("https://www.facebook.com/");
+			
+			driver.findElement(By.id("email")).sendKeys("9970848029");
+			driver.findElement(By.id("pass")).sendKeys("406721");
+			
+			driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+
+
+}
+}
